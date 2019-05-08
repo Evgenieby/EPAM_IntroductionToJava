3c96a6a814c24e38987ccbdbe5c1b07f12a4ad24
Evgeny <evgeny.zhukovsky@hotmail.com>
2019-05-09 02:03:51 +0300

    Restored project

---
+++ Task1.1-Equality-Inequality/src/by/epamtranning/action/CheckEqualityInequality.java
@@ -0,0 +1,15 @@
+package by.epamtranning.action;
+
+public class CheckEqualityInequality {
+
+    public CheckEqualityInequality() {
+    }
+
+    public boolean isEquality(int firstNum, int secondNum, int thirdNum) {
+        return firstNum == secondNum && firstNum == thirdNum;
+    }
+
+    public boolean isInequality(int firstNum, int secondNum, int thirdNum) {
+        return firstNum != secondNum && firstNum != thirdNum && secondNum != thirdNum;
+    }
+}
