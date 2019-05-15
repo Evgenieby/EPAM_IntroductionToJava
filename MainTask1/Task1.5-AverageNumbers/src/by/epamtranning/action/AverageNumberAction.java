78ceb21a451a87381124a2aad086c05cfbbe0465
Evgeny <evgeny.zhukovsky@hotmail.com>
2019-05-15 03:02:42 +0300

    Fix

---
+++ MainTask1/Task1.5-AverageNumbers/src/by/epamtranning/action/AverageNumberAction.java
@@ -0,0 +1,36 @@
+package by.epamtranning.action;
+
+public class AverageNumberAction {
+
+    private int totalDigits;
+    private int digit1;
+    private int digit2;
+    private int digit3;
+    private int digit4;
+    private int digit5;
+    private int digit6;
+
+    private void divisionNumber(int inputNumber) {
+        totalDigits = (int) Math.log10(inputNumber) + 1;
+        digit6 = inputNumber % 10;
+        digit5 = inputNumber / 10 % 10;
+        digit4 = inputNumber / 100 % 10;
+        digit3 = inputNumber / 1000 % 10;
+        digit2 = inputNumber / 10000 % 10;
+        digit1 = inputNumber / 100000 % 10;
+    }
+
+    public double calcArithmeticAverage(int inputNumber){
+        divisionNumber(inputNumber);
+        int digitSum = digit1 + digit2 + digit3 + digit4 + digit5 + digit6;
+
+        return ((double) digitSum / totalDigits);
+    }
+
+    public double calcGeometricAverage(int inputNumber){
+        divisionNumber(inputNumber);
+        int digitMultiply = digit1 * digit2 * digit3 * digit4 * digit5 * digit6;
+
+        return (Math.pow(digitMultiply, ((double) 1 / totalDigits)));
+    }
+}
---
+++ MainTask1/Task1.5-AverageNumbers/src/by/epamtranning/main/Application.java
@@ -0,0 +1,28 @@
+package by.epamtranning.main;
+
+import by.epamtranning.action.AverageNumberAction;
+import by.epamtranning.reader.DataConsoleReader;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.beginApp();
+    }
+
+    private void beginApp() {
+        DataConsoleReader reader = new DataConsoleReader();
+        System.out.println("Enter positive six-digit number:");
+        int inputNumber = reader.readIntNumber();
+
+        AverageNumberAction averageNumberAction = new AverageNumberAction();
+        int amountDigit = 6; // Количество цифр в числе
+
+        if (((int) Math.log10(inputNumber + 1) == amountDigit)) { // Проверка количества цифр в числе
+            System.out.println("The arithmetic average: " + averageNumberAction.calcArithmeticAverage(inputNumber));
+            System.out.println("The geometric average: " + averageNumberAction.calcGeometricAverage(inputNumber));
+        } else {
+            System.out.println("The number is not six-digit.");
+        }
+    }
+}
