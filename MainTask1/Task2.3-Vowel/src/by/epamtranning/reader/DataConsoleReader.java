d713f917ea5232eb1b910b8d4cb09d62566ff3ca
Evgeny <evgeny.zhukovsky@hotmail.com>
2019-05-15 03:23:54 +0300

    Fix

---
+++ MainTask1/Task2.3-Vowel/src/by/epamtranning/reader/DataConsoleReader.java
@@ -0,0 +1,12 @@
+package by.epamtranning.reader;
+
+import java.util.Scanner;
+
+public class DataConsoleReader {
+
+    public char readChar() {
+        Scanner scanner = new Scanner(System.in);
+
+        return scanner.next().charAt(0);
+    }
+}
